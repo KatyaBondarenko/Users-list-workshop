b77085a6a226c7acc65243b3e7002e7d96db7e53
Sanya711 <ptashnyk78@gmail.com>
2016-10-30 13:50:19 +0200

    test

--- src/create.js
+++ src/create.js
@@ -1,7 +1,64 @@
-export class CreatePage {
 
-}
 
 export default function initPage() {
-	alert('create page')
+
+    document.querySelector(".submit_form").addEventListener('submit',
+
+        function(event) {
+
+          event.preventDefault();
+
+          var fieldValue1 = document.querySelector('.Name').value;
+          var fieldValue2 = document.querySelector('.password').value;
+          var fieldValue3 = document.querySelector('.email').value;
+          var fieldValue4 = document.querySelector('.comment').value;
+
+          var payload = {
+
+          fieldValue1: fieldValue1,
+          fieldValue2: fieldValue2,
+          fieldValue3: fieldValue3,
+          fieldValue4: fieldValue4
+
+        };
+
+        var myHeaders = new Headers(); // создаём объект заголовков
+        myHeaders.append("Content-Type", "application/json");   /// добавляем заголовок Content-Type чтоб сказать серверу в каком формате данные передаём
+        var myInit = {
+                       method: 'POST', // указываем метод запроса
+                       headers: myHeaders,  // добавляем заголовки
+                       mode: 'cors',   // ставим режим кросс доменных запросов
+                       cache: 'default', // кеширование по умолчанию
+                       body: JSON.stringify({   /// добавляем данные к запросу
+                          name: fieldValue1,
+                          password: fieldValue2,
+                          email: fieldValue3,
+                          comment: fieldValue4
+                       })
+         };
+
+        var myRequest = new Request('http://193.111.63.76:3000/api/v1/Users', myInit); // создаём запрос
+
+        fetch(myRequest)   //говорим запросу выполнится
+        .then(function(response) {
+
+          if (response.status >= 200 && response.status < 300) {
+
+            return response.json(); /// парсим ответ от сервера в json
+
+          } else {
+
+               return Promise.reject();
+          }
+
+        })
+        .then(function(json) {
+              /// здесь ответ json от сервера
+              alert(JSON.stringify(json))
+              window.location = "http://localhost:3000/pages/list-page.html"
+        });
+
+
+
+        })
 }
--- src/list.js
+++ src/list.js
@@ -3,5 +3,64 @@ export class ListPage {
 }
 
 export default function initPage() {
-	alert('list page')
+	fetch('http://193.111.63.76:3000/api/v1/Users')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        var list = document.querySelector('.Table_header');
+
+        console.log(data);
+
+        var headerTitles = "";
+
+            for (var key in data[0]) {
+
+                headerTitles = headerTitles + "<th>" + key + "</th>";
+            }
+
+        var bodyData = "";
+
+        data.forEach(function(item){
+
+            bodyData = bodyData + "<tr>";
+
+            for (var key in item) {
+
+                var node = item[key];
+
+                bodyData = bodyData + "<td><pre>" + node + "</pre></td>";
+            }
+
+						bodyData+= "<td><button>Edit</button></td>";
+
+            bodyData+= `<td><button class = 'buttonDelete' id="${item._id}"> Delete </button></td>`;
+
+            bodyData+= "</tr>";
+
+        });
+
+        var bodyList = document.querySelector('.Table_body');
+
+        list.innerHTML = headerTitles;
+        bodyList.innerHTML = bodyData;
+
+
+
+ 			const nodes = document.querySelectorAll('.buttonDelete');
+      Array.from(nodes).forEach()
+      .addEventListener('click', (event) => {
+
+
+
+
+			}
+
+
+
+
+
+    });
+
 }
